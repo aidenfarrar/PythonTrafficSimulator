aa2c17f91ba104043b72513b84016a252630b0f0
Brandon <brandonwei112233@gmail.com>
2022-03-27 13:35:35 -0400

    added burst demo

--- src/trafficSimulator/vehicle.py
+++ src/trafficSimulator/vehicle.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Vehicle:
     def __init__(self, config={}):
         # Set default configuration
@@ -12,13 +13,13 @@ class Vehicle:
         # Calculate properties
         self.init_properties()
 
-    def set_default_config(self):    
-        self.l = 4
+    def set_default_config(self):
+        self.l = 4  # length of the car
         self.s0 = 4
         self.T = 1
-        self.v_max = 16.6
-        self.a_max = 1.44
-        self.b_max = 4.61
+        self.v_max = 16.6  # max velocity of the car
+        self.a_max = 1.44  # max acceleration of the car
+        self.b_max = 4.61  # max break speed of the car
 
         self.path = []
         self.current_road_index = 0
@@ -29,31 +30,32 @@ class Vehicle:
         self.stopped = False
 
     def init_properties(self):
-        self.sqrt_ab = 2*np.sqrt(self.a_max*self.b_max)
+        self.sqrt_ab = 2 * np.sqrt(self.a_max * self.b_max)
         self._v_max = self.v_max
 
     def update(self, lead, dt):
         # Update position and velocity
-        if self.v + self.a*dt < 0:
-            self.x -= 1/2*self.v*self.v/self.a
+        if self.v + self.a * dt < 0:
+            self.x -= 1 / 2 * self.v * self.v / self.a
             self.v = 0
         else:
-            self.v += self.a*dt
-            self.x += self.v*dt + self.a*dt*dt/2
-        
+            self.v += self.a * dt
+            self.x += self.v * dt + self.a * dt * dt / 2
+
         # Update acceleration
+
         alpha = 0
         if lead:
             delta_x = lead.x - self.x - lead.l
             delta_v = self.v - lead.v
 
-            alpha = (self.s0 + max(0, self.T*self.v + delta_v*self.v/self.sqrt_ab)) / delta_x
+            alpha = (self.s0 + max(0, self.T * self.v + delta_v * self.v / self.sqrt_ab)) / delta_x
 
-        self.a = self.a_max * (1-(self.v/self.v_max)**4 - alpha**2)
+        self.a = self.a_max * (1 - (self.v / self.v_max) ** 4 - alpha ** 2)
 
-        if self.stopped: 
-            self.a = -self.b_max*self.v/self.v_max
-        
+        if self.stopped:
+            self.a = -self.b_max * self.v / self.v_max
+
     def stop(self):
         self.stopped = True
 
@@ -65,5 +67,3 @@ class Vehicle:
 
     def unslow(self):
         self.v_max = self._v_max
-        
-
--- src/trafficSimulator/vehicle_generator.py
+++ src/trafficSimulator/vehicle_generator.py
@@ -1,13 +1,12 @@
 from .vehicle import Vehicle
 from numpy.random import randint
+from collections import deque
 
 class VehicleGenerator:
     def __init__(self, sim, config={}):
         self.sim = sim
-
         # Set default configurations
         self.set_default_config()
-
         # Update configurations
         for attr, val in config.items():
             setattr(self, attr, val)
@@ -17,11 +16,19 @@ class VehicleGenerator:
 
     def set_default_config(self):
         """Set default configuration"""
-        self.vehicle_rate = 20
+        self.poisson = False
+        self.vehicle_rate = 20 # rate for poisson dist
         self.vehicles = [
-            (1, {})
+            (1, {})  # (weight for generation, vehicle config)
         ]
         self.last_added_time = 0
+
+        self.burst_queue = deque()  # queue of generated vehicles
+        self.burst_freq = 50  # time between bursts
+        self.last_burst = 0  # time last burst was completed
+        self.min_burst = 5  # min number of vehicles in burst
+        self.max_burst = 5  # max number of vehicles in burst
+
 
     def init_properties(self):
         self.upcoming_vehicle = self.generate_vehicle()
@@ -37,16 +44,44 @@ class VehicleGenerator:
 
     def update(self):
         """Add vehicles"""
-        if self.sim.t - self.last_added_time >= 60 / self.vehicle_rate:
-            # If time elasped after last added vehicle is
-            # greater than vehicle_period; generate a vehicle
-            road = self.sim.roads[self.upcoming_vehicle.path[0]]      
-            if len(road.vehicles) == 0\
-               or road.vehicles[-1].x > self.upcoming_vehicle.s0 + self.upcoming_vehicle.l:
-                # If there is space for the generated vehicle; add it
-                self.upcoming_vehicle.time_added = self.sim.t
-                road.vehicles.append(self.upcoming_vehicle)
-                # Reset last_added_time and upcoming_vehicle
-                self.last_added_time = self.sim.t
-            self.upcoming_vehicle = self.generate_vehicle()
+        if self.poisson:
+            if self.sim.t - self.last_added_time >= 60 / self.vehicle_rate:
+                # If time elasped after last added vehicle is
+                # greater than vehicle_period; generate a vehicle
+                road = self.sim.roads[self.upcoming_vehicle.path[0]]
+                if len(road.vehicles) == 0\
+                   or road.vehicles[-1].x > self.upcoming_vehicle.s0 + self.upcoming_vehicle.l:
+                    # If there is space for the generated vehicle; add it
+                    self.upcoming_vehicle.time_added = self.sim.t
+                    road.vehicles.append(self.upcoming_vehicle)
+                    # Reset last_added_time and upcoming_vehicle
+                    self.last_added_time = self.sim.t
+                    self.upcoming_vehicle = self.generate_vehicle()
 
+        else:
+            # print(self.sim.t)
+            if len(self.burst_queue) > 0:
+                if self.sim.t - self.last_burst >= self.burst_freq:
+                    self.upcoming_vehicle = self.burst_queue[-1]
+                    road = self.sim.roads[self.upcoming_vehicle.path[0]]
+                    if len(road.vehicles) == 0\
+                       or road.vehicles[-1].x > self.upcoming_vehicle.s0 + self.upcoming_vehicle.l:
+                        # If there is space for the generated vehicle; add it
+                        self.upcoming_vehicle.time_added = self.sim.t
+                        road.vehicles.append(self.upcoming_vehicle)
+                        print(f"car sent : {self.sim.t}")
+                        self.burst_queue.pop()
+
+            else:
+                print(f"last burst time : {self.sim.t}")
+                self.last_burst = self.sim.t
+                self.bursty_gen(self.burst_queue)
+
+
+
+
+
+    def bursty_gen(self, burst_queue):
+        r = randint(self.min_burst, self.max_burst + 1)
+        for i in range(r):
+            burst_queue.append(self.generate_vehicle())
