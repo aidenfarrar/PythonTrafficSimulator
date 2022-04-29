a9c0bca06b89b3f8743bb45977eefbdfa84503ed
Aiden Farrar <aidencfarrar@gmail.com>
2022-04-29 17:05:38 -0400

    honestly not sure I did this a while ago

--- src/tests/gatech_test.py
+++ src/tests/gatech_test.py
@@ -7,9 +7,7 @@ from trafficSimulator import utils
 
 # Create simulation
 
-
 # Add multiple roads
-
 
 roads = []
 scale_factor = 15
@@ -17,6 +15,7 @@ offset = 40
 vertex_dict = {}
 reverse_vertex_dict = {}
 vertex_index = 0
+vertices_to_path_to = set()
 
 INF = 9999
 try:
@@ -25,8 +24,10 @@ try:
 except:
     num_vertices = 78
 
-graph = ones(shape=(num_vertices, num_vertices)) * INF
-path_matrix = [dict() for x in range(num_vertices)]
+walk_graph = ones(shape=(num_vertices, num_vertices)) * INF
+car_graph = ones(shape=(num_vertices, num_vertices)) * INF
+walk_path_matrix = [dict() for x in range(num_vertices)]
+car_path_matrix = [dict() for x in range(num_vertices)]
 
 with open("gt-model.txt", 'r') as file:
     file.readline()
@@ -40,25 +41,38 @@ with open("gt-model.txt", 'r') as file:
         a1, a2, b1, b2 = [int(x) * scale_factor + offset for x in points]
         # a1, a2, b1, b2 = [int(x) for x in points]
         a, b = (a1, a2), (b1, b2)
+
         if a not in vertex_dict.keys():
             vertex_dict[a] = vertex_index
             reverse_vertex_dict[vertex_index] = a
-            graph[vertex_index, vertex_index] = 0
+            walk_graph[vertex_index, vertex_index] = 0
+            if mode != 2: #mode 2 = walk / bike
+                vertices_to_path_to.add(a)
+                car_graph[vertex_index, vertex_index] = 0
             vertex_index += 1
         if b not in vertex_dict.keys():
             vertex_dict[b] = vertex_index
             reverse_vertex_dict[vertex_index] = b
-            graph[vertex_index, vertex_index] = 0
+            walk_graph[vertex_index, vertex_index] = 0
+            if mode != 2: #mode 2 = walk / bike
+                vertices_to_path_to.add(b)
+                car_graph[vertex_index, vertex_index] = 0
             vertex_index += 1
         ai, bi = vertex_dict[a], vertex_dict[b]
         dist = distance.euclidean(a, b)
-        graph[ai, bi] = dist
-        path_matrix[ai][bi] = [(ai, bi)]
+        walk_graph[ai, bi] = dist
+        if mode != 2:  # mode 2 = walk / bike
+            car_graph[ai, bi] = dist
+            car_path_matrix[ai][bi] = [(ai, bi)]
+        walk_path_matrix[ai][bi] = [(ai, bi)]
         roads.append((a, b, mode, mode == 2))
         if mode != 0:
             roads.append((b, a, mode, mode == 2))
-            graph[bi, ai] = dist
-            path_matrix[bi][ai] = [(bi, ai)]
+            walk_graph[bi, ai] = dist
+            walk_path_matrix[bi][ai] = [(bi, ai)]
+            if mode != 2:  # mode 2 = walk / bike
+                car_graph[bi, ai] = dist
+                car_path_matrix[bi][ai] = [(bi, ai)]
 
 with open('num_vertices', 'wb') as file:
     dump(vertex_index, file)
@@ -68,7 +82,7 @@ sim = Simulation(num_vertices=num_vertices, vertex_dict=vertex_dict)
 
 
 # path_matrix with shortest path destinations
-utils.floyd_warshall(graph, num_vertices, path_matrix)
+utils.floyd_warshall(walk_graph, car_graph, num_vertices, walk_path_matrix,  car_path_matrix)
 
 sim.create_roads(roads)
 
@@ -76,21 +90,34 @@ num_random_paths = 100
 vehicles = []
 
 # num vertices could be hard coded
-start_v_weights = utils.generate_weights(num_vertices)  # likelihood of choosing vertex as start
+start_v_weights = utils.generate_weights(len(vertices_to_path_to))  # likelihood of choosing vertex as start
 end_v_weights = utils.generate_weights(num_vertices)  # likelihood of choosing vertex as end
 
 weights = []
 
 while num_random_paths > 0:
-    start_vertex, end_vertex = choices(range(num_vertices), weights=start_v_weights)[0], choices(range(num_vertices), weights=end_v_weights)[0]  # chooses start road and end road
+    start_vertex, end_vertex = choices(list(vertices_to_path_to), weights=start_v_weights, k=2)  # chooses start road and end road
+    start_vertex = vertex_dict[start_vertex]
+    end_vertex = vertex_dict[end_vertex]
 
-    # prevent same start and end vertex
-    while start_vertex == end_vertex:
-        end_vertex = choices(range(num_vertices), weights=end_v_weights)[0]
+    #time estimate to get into car so people don't always take car
+    car_delay = 100
 
-    path = path_matrix[start_vertex][end_vertex]  # gets the shortest path from start road to end road from path_matrix
-    path = sim.path_converter(path)  # converts path coordinates to road
-    vehicles.append({"path": path, 'vehicle_type': utils.vehicle_selector(path)})
+    walk_length = walk_graph[start_vertex, end_vertex] / 2
+    car_length = car_graph[start_vertex, end_vertex] / 3 + car_delay
+
+    walk_path = walk_path_matrix[start_vertex][end_vertex]
+    car_path = car_path_matrix[start_vertex][end_vertex]
+
+    # gets the shortest path from start road to end road from path_matrix
+    if walk_length < car_length:
+        path = sim.path_converter(walk_path)
+        vehicles.append({"path": path, 'vehicle_type': utils.vehicle_selector('walk')})
+    else:
+        path = sim.path_converter(car_path)
+        vehicles.append({"path": path, 'vehicle_type': utils.vehicle_selector('drive')})
+    # converts path coordinates to road
+    # vehicles.append({"path": path, 'vehicle_type': utils.vehicle_selector(path)})
     weights.append(1)  # every car type is equally likely to be generated
     num_random_paths -= 1
 
--- src/trafficSimulator/utils.py
+++ src/trafficSimulator/utils.py
@@ -11,7 +11,7 @@ def generate_weights(num_vertices, config=None):
     # place holder
     return [1 for i in range(num_vertices)]
 
-def floyd_warshall(graph, num_vertices, path_matrix):
+def floyd_warshall(walk_graph, car_graph, num_vertices, walk_path_matrix, car_path_matrix):
     """
     populates path_matrix with shortest path destinations
     :param graph: graph of the road network
@@ -19,25 +19,28 @@ def floyd_warshall(graph, num_vertices, path_matrix):
     :param path_matrix: stores shortest paths from vertex i to j
     :return: updated path_matrix
     """
-    graph_copy = graph.copy()
+    # walk_graph_copy = walk_graph.copy()
+    # car_graph_copy = car_graph.copy()
     for k in range(num_vertices):
         for i in range(num_vertices):
             for j in range(num_vertices):
-                detour = graph_copy[i, k] + graph_copy[k, j]
-                if graph_copy[i, j] > detour:
-                    graph_copy[i, j] = detour
-                    path_matrix[i][j] = path_matrix[i][k] + path_matrix[k][j]
+                walk_detour = walk_graph[i, k] + walk_graph[k, j]
+                car_detour = car_graph[i, k] + car_graph[k, j]
+                if walk_graph[i, j] > walk_detour:
+                    walk_graph[i, j] = walk_detour
+                    walk_path_matrix[i][j] = walk_path_matrix[i][k] + walk_path_matrix[k][j]
+                if car_graph[i, j] > car_detour:
+                    car_graph[i, j] = car_detour
+                    car_path_matrix[i][j] = car_path_matrix[i][k] + car_path_matrix[k][j]
 
 
-def vehicle_selector(path):
+def vehicle_selector(trip_type):
     """
     selects vehicle type for paths
     :param path:  current path
     :return: name of vehicle type
     """
-    if len(path) > 10:
+    if trip_type == 'drive':
         return choices(["car", "bus"], weights=[50, 50])[0]  # placeholder 50 percent chance for bus to be generated
-    elif len(path) > 3:
-        return 'bike'
     else:
-        return 'walk'
+        return choices(["walk", "bike"], weights=[85, 15])[0]  # placeholder 30 percent chance for bike to be generated
